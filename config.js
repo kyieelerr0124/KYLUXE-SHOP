60db5233e0cb880d78aee7e91a026439d40adcbe
kyieelerr0124 <yoshitachi577@gmail.com>
2026-09-25 03:20:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.KYLUXE_CONFIG = {
   SUPABASE_URL: "https://uxuuwitfadkzlkmxjpnc.supabase.co/rest/v1/",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_3Knd1yVtsNx4Sy9lpUZw4Q_-Z3SrfSq"
+  SUPABASE_PUBLISHABLE_KEY: "https://uxuuwitfadkzlkmxjpnc.supabase.co/rest/v1/"
 };
